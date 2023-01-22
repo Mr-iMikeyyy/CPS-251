04c817dcf60357d4cb7840499cc5fbbcd1cdef07
Mike Mccullough <mikesawesomevids@gmail.com>
2023-01-22 17:03:37 -0500

    switched to view binding

--- examples/ActivityStateChange/app/src/main/java/mdmccullough/wccnet/edu/activitystatechange/MainActivity.kt
+++ examples/ActivityStateChange/app/src/main/java/mdmccullough/wccnet/edu/activitystatechange/MainActivity.kt
@@ -2,10 +2,50 @@ package mdmccullough.wccnet.edu.activitystatechange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mdmccullough.wccnet.edu.activitystatechange.databinding.ActivityMainBinding
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val TAG = "StateChange"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.i(TAG, "onCreate")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy")
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.i(TAG, "onSaveInstanceState")
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.i(TAG, "onRestoreInstanceState")
     }
 }
